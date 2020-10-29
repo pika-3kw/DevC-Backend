f62831767faa83c139a9946f493d0038ab4fb98f
pika-3kw <giangnam10a2@gmail.com>
2020-10-29 18:13:54 +0700

    Add Campaign Model And Modify User Model

---
+++ models/campaign.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const campaignSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  linkFacebook: [
+    {
+      type: {
+        type: String,
+        required: true,
+        enum: ['fanpage', 'group'],
+      },
+      parentId: {
+        type: String,
+        required: true,
+      },
+      postId: {
+        type: String,
+        required: true,
+      },
+      link: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model('Campain', campaignSchema);
